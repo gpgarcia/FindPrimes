aa783c8bb31ce8fcd5afdd8d3dd97fef82221e51
Gerardo Garcia <ggarcia1@vitas.com>
2025-02-27 14:44:24 -0500

    first attempt at Prichard

---
+++ FindPrimes/Pritchard.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPrimes
+{
+    class Pritchard : IPrime
+    {
+        private List<long> primes = null!;
+
+        public long N { get; init; }
+
+        public void Initialize()
+        {
+            long k = 0;
+            LinkedList<long> wheel = new();
+            wheel.AddLast(1);
+            long length = 2;
+            long p = 3;
+            primes = [2L];
+
+            while (p * p <= N)
+            {
+                if (length < N)
+                {
+                    (wheel,length) = Extend(wheel, length, Math.Min(p * length, N));
+                }
+                DeleteMultiple(wheel, p, length);
+                primes.Add(p);
+                k += 1;
+                p = Next(wheel, 1);
+                if (length < N)
+                {
+                    (wheel, length) = Extend(wheel,length, N);
+                }
+            }
+            wheel.Remove(1);
+            primes.AddRange(wheel);
+        }
+
+        private static void DeleteMultiple(LinkedList<long> W, long p, long length)
+        {
+            long w = p;
+            while (p * w <= length)
+            {
+                w = Next(W, w);
+            }
+            while (w > 1)
+            {
+                w = Prev(W, w);
+                W.Remove(p * w);
+            }
+        }
+
+
+        private static (LinkedList<long>, long len) Extend(LinkedList<long> W, long length, long n)
+        {
+            long w = 1;
+            long x = length + 1;
+            while(x <= n)
+            {
+                W.AddLast(x);
+                w = Next(W,w);
+                x = length+ w;
+            }
+            return (W, n);
+        }
+
+        private static long Next(LinkedList<long> w, long y)
+        {
+            return w.Find(y)?.Next?.Value ?? default;
+        }
+
+        private static long Prev(LinkedList<long> w, long y)
+        {
+            return w.Find(y)?.Previous?.Value ?? default;
+        }
+
+
+        public bool IsPrime(long n)
+        {
+            return primes.Contains(n);
+        }
+
+        public IEnumerable<long> GetPrimes()
+        {
+            return primes;
+        }
+    }
+}
--- FindPrimes/Program.cs
+++ FindPrimes/Program.cs
@@ -7,76 +7,84 @@ namespace FindPrimes
     {
         static void Main(string[] _)
         {
-            bool test = false;
+            bool test = true;
             bool bench = !test;
             if (test)
             {
                 Console.WriteLine($"int.MaxValue=   {int.MaxValue}");
                 Console.WriteLine($"Array.MaxLength={Array.MaxLength}");
-                RunCount<Definition>(1);
-                RunSearch<Definition>(100);
-                RunCount<Definition>(1_000);
+
+                //RunCount<Definition>(1);
+                //RunSearch<Definition>(100);
+                //RunCount<Definition>(1_000);
 
 
-                RunSearch<Eratosthenes1>(100);
-                RunCount<Eratosthenes1>(1_000);
+                //RunSearch<Eratosthenes1>(100);
+                //RunCount<Eratosthenes1>(1_000);
 
-                RunSearch<Eratosthenes2>(100);
-                RunCount<Eratosthenes2>(1_000);
-                RunCount<Eratosthenes2>(10_000);
+                //RunSearch<Eratosthenes2>(100);
+                //RunCount<Eratosthenes2>(1_000);
+                //RunCount<Eratosthenes2>(10_000);
 
-                RunSearch<Eratosthenes3>(100);
-                RunCount<Eratosthenes3>(1_000);
-                RunCount<Eratosthenes3>(10_000);
-                try
-                {
-                    RunCount<Eratosthenes3>(2_147_483_591 + 1);
-                }
-                catch (OutOfMemoryException ex)
-                {
-                    Console.WriteLine($"N={2_147_483_591 + 1}, {ex.Message}");
-                }
-                RunCount<Eratosthenes4>(1);
-                RunSearch<Eratosthenes4>(100);
-                RunCount<Eratosthenes4>(1_000);
-                RunCount<Eratosthenes4>(10_000);
-                RunCount<Eratosthenes4>(10_000_000);
-                //RunCount<Eratosthenes4>(2_147_483_591 + 1);
-                //RunCount<Eratosthenes4>(2L * (long)Array.MaxLength +1L);
-                try
-                {
-                    RunCount<Eratosthenes4>(2L * (long)Array.MaxLength + 2L);
-                }
-                catch (OutOfMemoryException ex)
-                {
-                    Console.WriteLine($"N={2L * (long)Array.MaxLength + 2L}, {ex.Message}");
-                }
+                //RunSearch<Eratosthenes3>(100);
+                //RunCount<Eratosthenes3>(1_000);
+                //RunCount<Eratosthenes3>(10_000);
+                //try
+                //{
+                //    RunCount<Eratosthenes3>(2_147_483_591 + 1);
+                //}
+                //catch (OutOfMemoryException ex)
+                //{
+                //    Console.WriteLine($"N={2_147_483_591 + 1}, {ex.Message}");
+                //}
 
-                RunSearch<EulerList>(100);
-                RunCount<EulerList>(1_000);
-                RunCount<EulerList>(10_000);
+                //RunCount<Eratosthenes4>(1);
+                //RunSearch<Eratosthenes4>(100);
+                //RunCount<Eratosthenes4>(1_000);
+                //RunCount<Eratosthenes4>(10_000);
+                //RunCount<Eratosthenes4>(10_000_000);
+                ////RunCount<Eratosthenes4>(2_147_483_591 + 1);
+                ////RunCount<Eratosthenes4>(2L * (long)Array.MaxLength +1L);
+                //try
+                //{
+                //    RunCount<Eratosthenes4>(2L * (long)Array.MaxLength + 2L);
+                //}
+                //catch (OutOfMemoryException ex)
+                //{
+                //    Console.WriteLine($"N={2L * (long)Array.MaxLength + 2L}, {ex.Message}");
+                //}
 
-                RunSearch<EratosOdd>(100);
-                RunCount<EratosOdd>(1_000);
-                RunCount<EratosOdd>(10_000);
-                RunCount<EratosOdd>(10_000_000);
-                //RunCount<EratosOdd>(Array.MaxLength + 1);
-                //RunCount<EratosOdd>(2L * (long)Array.MaxLength + 1L);
-                //RunCount<EratosOdd>(2L * (long)int.MaxValue + 1L);
-                try
-                {
-                    RunCount<EratosOdd>(2L * (long)int.MaxValue + 2L);
-                }
-                catch (ArgumentOutOfRangeException ex)
-                {
-                    Console.WriteLine($"N={2L * (long)int.MaxValue + 2L}, {ex.Message}");
-                }
+                RunCount<Pritchard>(1);
+                RunSearch<Pritchard>(100);
+                RunCount<Pritchard>(1_000);
+                RunCount<Pritchard>(10_000);
+                //RunCount<Pritchard>(10_000_000);
 
-                RunSearch<SegmentedSieve>(100);
-                RunCount<SegmentedSieve>(1_000);
-                RunCount<SegmentedSieve>(10_000);
-                RunCount<SegmentedSieve>(10_000_000);
-                //RunCount<SegmentedSieve>(2L * (long)int.MaxValue + 1L);
+                //RunSearch<EulerList>(100);
+                //RunCount<EulerList>(1_000);
+                //RunCount<EulerList>(10_000);
+
+                //RunSearch<EratosOdd>(100);
+                //RunCount<EratosOdd>(1_000);
+                //RunCount<EratosOdd>(10_000);
+                //RunCount<EratosOdd>(10_000_000);
+                ////RunCount<EratosOdd>(Array.MaxLength + 1);
+                ////RunCount<EratosOdd>(2L * (long)Array.MaxLength + 1L);
+                ////RunCount<EratosOdd>(2L * (long)int.MaxValue + 1L);
+                //try
+                //{
+                //    RunCount<EratosOdd>(2L * (long)int.MaxValue + 2L);
+                //}
+                //catch (ArgumentOutOfRangeException ex)
+                //{
+                //    Console.WriteLine($"N={2L * (long)int.MaxValue + 2L}, {ex.Message}");
+                //}
+
+                //RunSearch<SegmentedSieve>(100);
+                //RunCount<SegmentedSieve>(1_000);
+                //RunCount<SegmentedSieve>(10_000);
+                //RunCount<SegmentedSieve>(10_000_000);
+                ////RunCount<SegmentedSieve>(2L * (long)int.MaxValue + 1L);
 
             }
             if (bench)
